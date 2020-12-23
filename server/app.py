dea1e3ff8ec4ca1e31061821a5426bbb5ae8c59d
Ben Paulson <30847493+ben-paulson@users.noreply.github.com>
2020-12-22 20:27:23 -1000

    Require login to access the map

--- server/app.py
+++ server/app.py
@@ -1,17 +1,59 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
 import config
 import json
+import os.path
 
 app = Flask(__name__)
-app.secret_key = config.get_config('static/config.js')['secret_key']
+cfg = config.get_config('static/config.js')
+app.secret_key = cfg['secret_key']
 new_points = []
+
+def verify_user(usr, pwd):
+    """Verifies that the username and password are correct
+    Arguments:
+        usr (str): The username
+        pwd (str): The password
+    Returns:
+        bool: True if user/pass is correct, False otherwise
+    """
+    if cfg['username'] == usr and cfg['password'] == pwd:
+        return True
+    return False
+
+def is_logged_in():
+    """Checks if the user is logged in or not.
+    Returns:
+        bool: True if logged in, False if not
+    """
+    return 'verified' in session and session['verified']
 
 @app.route('/')
 def map():
+    """Main page, displays the map if logged in.
+    """
+    if not is_logged_in():
+        return redirect(url_for('login'))
     return render_template('map.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Redirects to this page if user is not logged in.
+    """
+    if request.method == "POST":
+        if verify_user(request.form['user'], request.form['pass']):
+             session['verified'] = True
+             return redirect(url_for("map"))
+        msg = "Could not authenticate. Please try again."
+        return render_template("login.html", message=msg)
+    else:
+        return render_template("login.html", message="")
 
 @app.route('/gpsupdate', methods=['GET', 'POST'])
 def update():
+    """New position data is sent via a POST request by the GPS.
+    The server GETs the new data every xxxx milliseconds
+    and updates the map with the new data.
+    """
     global new_points
     if request.method == 'POST':
         point = request.get_json()
